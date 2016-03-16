6c49d9f90c5a477aeedcf22e148082ad50130875
niesen918 <niesen918@gmail.com>
2016-03-16 11:51:57 +0800

    ServiceOrder2

---
+++ app/src/main/java/com/cmbb/smartkids/activity/order/v2/PayConfirm.java
@@ -0,0 +1,24 @@
+package com.cmbb.smartkids.activity.order.v2;
+
+import android.os.Bundle;
+
+import com.cmbb.smartkids.R;
+import com.cmbb.smartkids.base.BaseActivity;
+
+/**
+ * 项目名称：LovelyBaby
+ * 类描述：
+ * 创建人：N.Sun
+ * 创建时间：16/3/16 上午11:47
+ */
+public class PayConfirm extends BaseActivity {
+    @Override
+    protected void init(Bundle savedInstanceState) {
+
+    }
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.activity_pay_comfirm_layout;
+    }
+}
